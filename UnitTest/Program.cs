82f70f29a668c3ecc507c329965285361657aa91
Ron Michael <kiritoneesan@gmail.com>
2017-07-04 02:56:00 +0800

    Successfuly tested the EncryptFile() and DecryptFile() methods of the Cipher utility class.

--- UnitTest/Program.cs
+++ UnitTest/Program.cs
@@ -12,11 +12,26 @@ namespace UnitTest
     {
         private static void Main(string[] args)
         {
+            string clearFile = @"G:\TestDirectory\kdt.txt";
+            string encryptedFile = @"G:\TestDirectory\kdt.txt.cry";
+
+            Cipher.EncryptFile(clearFile, encryptedFile, "kiditouGami161_");
+            Cipher.DecryptFile(encryptedFile, @"G:\TestDirectory\decrypted.txt", "kiditouGami161_");
+        }
+
+        private static void EncryptDecryptStrings()
+        {
             string clearString = "The quick brown fox jumps over the lazy dog.";
-            string encryptedString = Cipher.EncryptString(clearString, "AyyLmaoXD");
+            string encryptedString = Cipher.EncryptString(clearString, clearString);
 
             Console.WriteLine("Clear Text:\t{0}", clearString);
+            Console.WriteLine();
             Console.WriteLine("Encrypted Text:\t{0}", encryptedString);
+            Console.WriteLine();
+
+            string decryptedString = Cipher.DecryptString(encryptedString, clearString);
+
+            Console.WriteLine("Decrypted Text:\t{0}", decryptedString);
         }
     }
 }
