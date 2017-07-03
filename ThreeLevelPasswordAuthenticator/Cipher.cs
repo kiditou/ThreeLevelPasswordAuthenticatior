f3b499797cc2d8dc7e30c859e0b0c16ebc071e4a
Ron Michael <kiritoneesan@gmail.com>
2017-07-04 01:44:17 +0800

    Added Encrypt() and EncryptString() methods to the Cipher utility class. Added UnitTest project.

--- ThreeLevelPasswordAuthenticator/Cipher.cs
+++ ThreeLevelPasswordAuthenticator/Cipher.cs
@@ -4,10 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+using System.Security.Cryptography;
+
 namespace ThreeLevelPasswordAuthenticator
 {
-    class Cipher
+    public class Cipher
     {
+        /// <summary>
+        /// Encrypts an array of data using a Key and IV.
+        /// </summary>
+        /// <param name="clearData">Data to be encrypted.</param>
+        /// <param name="key">Key used for the symmetric algorithm.</param>
+        /// <param name="IV">Initialization vector for the symmetric algorithm.</param>
+        /// <returns>Returns an array of encrypted bytes.</returns>
+        public static byte[] Encrypt(byte[] clearData, byte[] key, byte[] IV)
+        {
+            MemoryStream ms = new MemoryStream();
 
+            Rijndael alg = Rijndael.Create();
+            alg.Key = key;
+            alg.IV = IV;
+
+            CryptoStream cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(clearData, 0, clearData.Length);
+            cs.Close();
+
+            return ms.ToArray();
+        }
+
+        public static byte[] Encrypt(byte[] clearData, string password)
+        {
+            byte[] rgbSalt = Encoding.Unicode.GetBytes("The quick brown fox jumps over the lazy dog.");
+            PasswordDeriveBytes pdb = new PasswordDeriveBytes(password, rgbSalt);
+
+            return Encrypt(clearData, pdb.GetBytes(32), pdb.GetBytes(16));
+        }
+
+        public static string EncryptString(string clearString, string password)
+        {
+            byte[] clearData = Encoding.Unicode.GetBytes(clearString);
+
+            byte[] rgbSalt = Encoding.Unicode.GetBytes("The quick brown fox jumps over the lazy dog.");
+            PasswordDeriveBytes pdb = new PasswordDeriveBytes(password, rgbSalt);
+
+            byte[] encryptedData = Encrypt(clearData, pdb.GetBytes(32), pdb.GetBytes(16));
+
+            return Convert.ToBase64String(encryptedData);
+        }
     }
 }
