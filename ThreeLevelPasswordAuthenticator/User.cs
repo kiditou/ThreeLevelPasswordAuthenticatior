4d755887bb0d5373beb616fe3e0d3b27b5a43090
Ron Michael <kiritoneesan@gmail.com>
2017-07-05 03:05:46 +0800

    Added WriteToFile() and updated the ReadToFile() methods.

--- ThreeLevelPasswordAuthenticator/User.cs
+++ ThreeLevelPasswordAuthenticator/User.cs
@@ -9,8 +9,21 @@ namespace kdt.ThreeLevelPasswordAuthenticator.UserManagement
     public class User
     {
         public string Name { get; set; }
-        public string Username { get; set; }
+        public string Passphrase { get; set; }
         public string Password { get; set; }
+        public string Username { get; set; }
         // TODO: Add more user details.
+
+        public User()
+        {
+        }
+
+        public User(string name, string username, string passphrase, string password)
+        {
+            this.Name = name;
+            this.Username = username;
+            this.Passphrase = passphrase;
+            this.Password = password;
+        }
     }
 }
--- ThreeLevelPasswordAuthenticator/UserManager.cs
+++ ThreeLevelPasswordAuthenticator/UserManager.cs
@@ -10,7 +10,7 @@ namespace kdt.ThreeLevelPasswordAuthenticator.UserManagement
 {
     internal class UserManager
     {
-        public static User ReadFile(string filePath)
+        public static User ReadFromFile(string filePath)
         {
             string[] fileContents = null;
             if (File.Exists(filePath))
@@ -19,14 +19,18 @@ namespace kdt.ThreeLevelPasswordAuthenticator.UserManagement
             }
 
             string[] currentLine = null;
-            string optionTitle;
-            string optionContent;
+            string optionTitle = null;
+            string optionContent = null;
+            StringSplitOptions splitOption = StringSplitOptions.RemoveEmptyEntries;
+            string[] lineSeparator = new string[] { " = " };
+
+            User user = new User();
+
             if (fileContents != null)
             {
                 for (int l = 0; l < fileContents.Length; l++)
                 {
-                    currentLine = fileContents[l].Split(new string[] { " = " },
-                                                        StringSplitOptions.RemoveEmptyEntries);
+                    currentLine = fileContents[l].Split(lineSeparator, splitOption);
 
                     optionTitle = currentLine[0];
                     optionContent = currentLine[1];
@@ -38,13 +42,83 @@ namespace kdt.ThreeLevelPasswordAuthenticator.UserManagement
                             case "Name":
                                 if (optionContent.Length != 0)
                                 {
+                                    user.Name = optionContent;
+                                }
+                                else
+                                {
+                                    user.Name = null;
+                                }
+                                break;
+
+                            case "Passphrase":
+                                if (optionContent.Length != 0)
+                                {
+                                    user.Passphrase = optionContent;
+                                }
+                                else
+                                {
+                                    user.Passphrase = null;
+                                }
+                                break;
+
+                            case "Passpword":
+                                if (optionContent.Length != 0)
+                                {
+                                    user.Password = optionContent;
+                                }
+                                else
+                                {
+                                    user.Password = null;
+                                }
+                                break;
+
+                            case "Username":
+                                if (optionContent.Length != 0)
+                                {
+                                    user.Username = optionContent;
+                                }
+                                else
+                                {
+                                    user.Username = null;
                                 }
                                 break;
                         }
                     }
                 }
             }
+            else
+            {
+                user.Name = null;
+                user.Passphrase = null;
+                user.Password = null;
+                user.Username = null;
+            }
+
             return new User();
         }
+
+        public static void WriteToFile(User user)
+        {
+            string dateNow = DateTime.Now.ToShortDateString();
+            string timeNow = DateTime.Now.ToShortTimeString();
+
+            string[] fileContents = new string[]
+            {
+                string.Format("# User configuration for {0}.",user.Username),
+                string.Format("# Last updated on {0}, {0}", dateNow, timeNow),
+                "",
+                string.Format("Name = {0}", user.Name),
+                string.Format("Passphrase = {0}", user.Passphrase),
+                string.Format("Password = {0}", user.Password),
+                string.Format("Username = {0}", user.Username)
+            };
+
+            string file = string.Format(@"Users\{0}.usr", user.Username);
+
+            File.Create(file);
+            File.WriteAllLines(file, fileContents);
+        }
+
+        // TODO: Add AppendToFile() method.
     }
 }
